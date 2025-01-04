d8714cee59d19a40a2fc1ce41e663a7d18681104
fobic <sabychakraborty08@gmail.com>
2025-01-04 16:30:36 +0530

    feat(server) : added fastapi - websocket service to provide real time response.

--- server/main.py
+++ server/main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+import asyncio
+from fastapi import FastAPI, WebSocket
 
 from model.chat_body import ChatBody
 from services.llm import LLMService
@@ -10,6 +11,27 @@ app = FastAPI()
 search = SearchService()
 sort = SortSourceService()
 llm = LLMService()
+
+@app.websocket("/ws/chat")
+async def websocket_chat_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    
+    try:
+        await asyncio.sleep(0.1)
+        data = await websocket.receive_json()
+        query = data.get("query")
+        results = search.web_search(query)
+        sorted = sort.sort_sources(query, results)
+        await asyncio.sleep(0.1)
+        await websocket.send_json({"type": "search", "data": sorted})
+        for chunk in llm.generate_response(query, sorted):
+            await asyncio.sleep(0.1)
+            await websocket.send_json({"type": "content", "data":chunk})
+            
+    except:
+        print("Error")
+    finally:
+        await websocket.close()
 
 @app.post("/chat")
 def chat_endpoint(body: ChatBody):
